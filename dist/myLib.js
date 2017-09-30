e0e92baafffd0468d2a9d3bacdbe7ec54a4ec8d2
Felix Rilling <contact@f-rilling.com>
2017-09-30 12:00:40 +0200

    updated babel integration

--- dist/myLib.js
+++ dist/myLib.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var myLib = function (exports) {
   'use strict';
   /**
--- tasks/jsDist.js
+++ tasks/jsDist.js
@@ -4,6 +4,18 @@ const babel = require("babel-core");
 const uglify = require("uglify-es");
 const bundle = require("./lib/bundle");
 
+const OPTIONS_BABEL = {
+    compact: false,
+    ast: false,
+    presets: [
+        ["env", {
+            targets: {
+                browsers: "chrome >= 58",
+            }
+        }]
+    ]
+};
+
 bundle([{
     id: "es",
     ext: ".esm",
@@ -18,16 +30,10 @@ bundle([{
     id: "iife",
     ext: "",
     name: "IIFE",
-    fn: code => babel.transform(code, {
-        compact: false
-    }).code
+    fn: code => babel.transform(code, OPTIONS_BABEL).code
 }, {
     id: "iife",
     ext: ".min",
     name: "IIFE:min",
-    fn: code => uglify.minify(
-        babel.transform(code, {
-            compact: false
-        }).code
-    ).code
+    fn: code => uglify.minify(babel.transform(code, OPTIONS_BABEL).code).code
 }], []);
