b87fae4922fd36a5b7825e9b9f2b5a1bae9a4607
Felix Rilling <contact@f-rilling.com>
2017-10-13 09:40:13 +0200

    updated test

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,10 +1,7 @@
 "use strict";
 
-const packageJson = require("../package.json");
-const {
-    DIR_DIST
-} = require("../tasks/lib/constants");
-const myLib = require(`../${DIR_DIST}/${packageJson.namespace.file}.common.js`);
+const CONSTANTS = require("../package.json").constants;
+const myLib = require(`../${CONSTANTS.dirBase.output}/${CONSTANTS.js.namespace.file}.common.js`);
 
 describe("Main test", function () {
     it("Normal case", function () {
