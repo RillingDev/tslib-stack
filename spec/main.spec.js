1727bf1a0c471c2d8dc59fba46fa6156278b4709
Felix Rilling <felix@f-rilling.com>
2017-12-29 11:11:31 -0500

    updated test

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,10 +1,9 @@
 "use strict";
 
-const CONSTANTS = require("../package.json").constants;
-const myLib = require(`../${CONSTANTS.dirBase.output}/${CONSTANTS.js.namespace.file}.common.js`);
+const myLib = require(`../dist/myLib.common`);
 
-describe("Main test", function () {
-    it("Normal case", function () {
+describe("Main test", () => {
+    it("Normal case", () => {
         expect(myLib.add(1, 1)).toBe(2);
     });
 });
